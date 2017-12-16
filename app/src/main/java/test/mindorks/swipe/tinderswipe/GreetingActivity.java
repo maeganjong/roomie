020f098614acb8d9d838613bb30c206ac980c00d
Maegan <maegan.jong@github.com>
2017-12-16 09:25:42 -0600

    create JSON object in sign up activity and login for greeting activity

--- app/src/main/java/test/mindorks/swipe/tinderswipe/GreetingActivity.java
+++ app/src/main/java/test/mindorks/swipe/tinderswipe/GreetingActivity.java
@@ -1,5 +1,6 @@
 package test.mindorks.swipe.tinderswipe;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -11,38 +12,53 @@ import android.widget.Toast;
 
 public class GreetingActivity extends AppCompatActivity {
 
-    Button logIn = (Button) findViewById(R.id.logbtn);
-    Button signUp = (Button) findViewById(R.id.signbtn);
-    EditText username = (EditText) findViewById(R.id.userName);
-    EditText password = (EditText) findViewById(R.id.password);
+    Button logIn;
+    Button signUp;
+    EditText usernamet;
+    String username;
+    EditText passwordt;
+    String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_greeting);
 
+        logIn = (Button) findViewById(R.id.logbtn);
+        signUp = (Button) findViewById(R.id.signbtn);
+        usernamet = (EditText) findViewById(R.id.userName);
+        username = usernamet.getText().toString();
+        passwordt = (EditText) findViewById(R.id.password);
+        password = passwordt.getText().toString();
+
         logIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (username.equals("")&&password.equals("")){
+                Context context1 = GreetingActivity.this.getApplicationContext();
+                for(Profile profile : Utils.loadProfiles(context1)) {
 
-                Intent intent = new Intent (GreetingActivity.this, MainActivity.class);
-                startActivity(intent);}
+                    if (username.equals(profile.getUsername()) && password.equals(profile.getPassword())) {
 
-                else{
-                    Context context = getApplicationContext();
-                    CharSequence text = "Username or password is incorrect";
-                    int duration = Toast.LENGTH_SHORT;
-
-                    Toast toast = Toast.makeText(context, text, duration);
-                    toast.show();
+                        Intent intent = new Intent(GreetingActivity.this, MainActivity.class);
+                        startActivity(intent);
+                    }
                 }
+
+                Context context = getApplicationContext();
+                CharSequence text = "Username or password is incorrect";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+
             }
         });
+
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Intent intent = new Intent (t)
+                Intent intent = new Intent(GreetingActivity.this, SignUpActivity.class);
+                startActivity(intent);
             }
         });
     }
--- app/src/main/java/test/mindorks/swipe/tinderswipe/SignUpActivity.java
+++ app/src/main/java/test/mindorks/swipe/tinderswipe/SignUpActivity.java
@@ -1,13 +1,58 @@
 package test.mindorks.swipe.tinderswipe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.json.JSONObject;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    EditText name, age, imageURL, description, username, password;
+    Button accountbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.passWord);
+        name = (EditText) findViewById(R.id.name);
+        age = (EditText) findViewById(R.id.age);
+        imageURL = (EditText) findViewById(R.id.url);
+        description = (EditText) findViewById(R.id.description);
+        accountbtn = (Button) findViewById(R.id.accountbtn);
+
+        accountbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                JSONObject jObj = new JSONObject();
+                try {
+
+                    jObj.put("name", name.getText().toString());
+                    jObj.put("age",  age.getText().toString());
+                    jObj.put("url",  imageURL.getText().toString());
+                    jObj.put("description",  description.getText().toString());
+                    jObj.put("username",  username.getText().toString());
+                    jObj.put("password",  password.getText().toString());
+                    jObj.put("college", "0");
+
+                    //place jObj into local profiles JSONArray
+
+                    Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                    startActivity(intent);
+
+                } catch (Exception e) {
+                    System.out.println("Error:" + e);
+                }
+
+            }
+        });
+
+
     }
 }
